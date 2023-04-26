aadc9fb1080993abcfb2b41f5fa2707fa9536098
Pandorolo <52629287+Pandorolo@users.noreply.github.com>
2023-04-26 09:26:37 +0200

    first commit

---
+++ Assets/PlaceMonster.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceMonster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public GameObject monsterPrefab;
+mostro GameObject privato ;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        private  bool  CanPlaceMonster ()
+{
+  ritorno mostro == nullo ;
+}
+    }
+}
