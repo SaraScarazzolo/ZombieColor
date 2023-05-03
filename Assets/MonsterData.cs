ae803726f443f7ae0afc54bf672f79b12074e628
davidlore2004 <david.lore@isitgo.it>
2023-05-03 09:53:23 +0200

    Monster

---
+++ Assets/MonsterData.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MonsterLevel
+{
+  public int cost;
+  public GameObject visualization;
+}
+
+
+public class MonsterData : MonoBehaviour
+{
+    public List<MonsterLevel> levels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
