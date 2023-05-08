49bebe2c5b15159a7049e3fc617c4fca0d785708
Pandorolo <52629287+Pandorolo@users.noreply.github.com>
2023-05-08 17:49:00 +0200

    Finished Tutorial Part 1

--- Assets/Scripts/PlaceMonster.cs
+++ Assets/Scripts/PlaceMonster.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 
 
 public class PlaceMonster : MonoBehaviour {
-	public GameObject monsterPrefab;
-	private GameObject monster;
+		public GameObject monsterPrefab;
+		private GameObject monster;
+		private GameManagerBehavior gameManager;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
     }
 
     // Update is called once per frame
@@ -21,17 +22,39 @@ public class PlaceMonster : MonoBehaviour {
     }
 
     private bool CanPlaceMonster() {
- 		return monster == null;
-	}
+ 				int cost = monsterPrefab.GetComponent<MonsterData>().levels[0].cost;
+				return monster == null && gameManager.Gold >= cost;
+		}
 
-	void OnMouseUp() {
-  		if (CanPlaceMonster()) {
-    		monster = (GameObject) 
-      			Instantiate(monsterPrefab, transform.position, Quaternion.identity);
-    		AudioSource audioSource = gameObject.GetComponent<AudioSource>();
-    		audioSource.PlayOneShot(audioSource.clip);
-  		}
-	}
+		void OnMouseUp() {
+				if (CanPlaceMonster()) {
+					monster = (GameObject) 
+							Instantiate(monsterPrefab, transform.position, Quaternion.identity);
+					AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+					audioSource.PlayOneShot(audioSource.clip);
+				}
+				else if (CanUpgradeMonster())
+				{
+					monster.GetComponent<MonsterData>().IncreaseLevel();
+					AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+					audioSource.PlayOneShot(audioSource.clip);
+					gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
+				}
+		}
+
+		private bool CanUpgradeMonster()
+		{
+			if (monster != null)
+			{
+				MonsterData monsterData = monster.GetComponent<MonsterData>();
+				MonsterLevel nextLevel = monsterData.GetNextLevel();
+				if (nextLevel != null)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
 }
 
 
