ffe290d5a4e79f0c977333d27053ffee1b777c19
Massi Nicola <nicola.massi@isitgo.it>
2023-05-08 09:49:05 +0200

    Final Commit

--- Tower_defence/Assets/PlaceMonster.cs
+++ Tower_defence/Assets/PlaceMonster.cs
@@ -2,47 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class PlaceMonster : MonoBehaviour{
 
+  private GameManagerBehavior gameManager;
+  public GameObject monsterPrefab;
+  private GameObject monster;
 
-public class PlaceMonster : MonoBehaviour
-{
+  // Start is called before the first frame update
+  void Start(){
+    gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
+  }
 
-    public GameObject monsterPrefab;
-    private GameObject monster;
+  // Update is called once per frame
+  void Update(){
+      
+  }
 
-    // Start is called before the first frame update
-    void Start()
+  private bool CanUpgradeMonster(){
+    if (monster != null)
     {
-        
+      MonsterData monsterData = monster.GetComponent<MonsterData>();
+      MonsterLevel nextLevel = monsterData.GetNextLevel();
+
+      if (nextLevel != null){
+        return gameManager.Gold >= nextLevel.cost;
+      }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    return false;
+  }
+
+  void OnMouseUp(){
+    if (CanPlaceMonster()){
+    monster = (GameObject) 
+    Instantiate(monsterPrefab, transform.position, Quaternion.identity);
+    AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+    audioSource.PlayOneShot(audioSource.clip);
+    gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
     }
 
-    //1
-    void OnMouseUp()
-    {
-        //2
-        if (CanPlaceMonster())
-        {
-        //3
-        monster = (GameObject) 
-            Instantiate(monsterPrefab, transform.position, Quaternion.identity);
-        //4
-        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
-        audioSource.PlayOneShot(audioSource.clip);
-
-        // TODO: Deduct gold
-        }
+    else if (CanUpgradeMonster()){
+      monster.GetComponent<MonsterData>().IncreaseLevel();
+      AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+      audioSource.PlayOneShot(audioSource.clip);
+      gameManager.Gold -= monster.GetComponent<MonsterData>().CurrentLevel.cost;
     }
+  }
 
-private bool CanPlaceMonster()
-{
-  return monster == null;
-}
-
+  private bool CanPlaceMonster(){
+    int cost = monsterPrefab.GetComponent<MonsterData>().levels[0].cost;
+    return monster == null && gameManager.Gold >= cost;
+  }
 }
 
