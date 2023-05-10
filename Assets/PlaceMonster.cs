9f9e5bd111fafcbb94e8b15e740bb235da5ef15a
Vecchielia <michele.visintin1@isitgo.it>
2023-05-10 10:05:13 +0200

    modifiche fino a inserimento mostri

---
+++ Assets/PlaceMonster.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceMonster : MonoBehaviour
+{
+    public GameObject monsterPrefab;
+    private GameObject monster;
+
+    private bool CanPlaceMonster ()
+    {
+        return monster == null;
+    }
+
+    //1
+    void OnMouseUp()
+    {
+        //2
+        if (CanPlaceMonster())
+        {
+            //3
+            monster = (GameObject)
+                Instantiate(monsterPrefab, transform.position, Quaternion.identity);
+            //4
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+
+            // TODO: Deduct gold
+
+        }
+    }
+}
