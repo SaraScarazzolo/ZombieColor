a040d94298707db2e1dbe94d34b7a381b210c1b9
Daniel Liut <daniel.liut@isitgo.it>
2023-05-10 10:14:10 +0200

    Commit finale
    Parte 1 finita e parte 2 a metà

---
+++ Assets/GameManagerBehavior.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManagerBehavior : MonoBehaviour
+{
+    public Text healthLabel;
+    public GameObject[] healthIndicator;
+    
+    public bool gameOver = false;
+    public Text waveLabel;
+    public GameObject[] nextWaveLabels;
+
+    public Text goldLabel;
+    private int gold;
+    public int Gold
+    {
+        get
+        {
+            return gold;
+        }
+        set
+        {
+            gold = value;
+            goldLabel.GetComponent<Text>().text = "GOLD: " + gold;
+        }
+    }
+
+    private int wave;
+    public int Wave
+    {
+    get
+    {
+        return wave;
+    }
+    set
+    {
+        wave = value;
+        if (!gameOver)
+        {
+        for (int i = 0; i < nextWaveLabels.Length; i++)
+        {
+            nextWaveLabels[i].GetComponent<Animator>().SetTrigger("nextWave");
+        }
+        }
+        waveLabel.text = "WAVE: " + (wave + 1);
+    }
+    }
+
+    private int health;
+    public int Health
+    {
+    get
+    {
+        return health;
+    }
+    set
+    {
+        // 1
+        if (value < health)
+        {
+        Camera.main.GetComponent<CameraShake>().Shake();
+        }
+        // 2
+        health = value;
+        healthLabel.text = "HEALTH: " + health;
+        // 3
+        if (health <= 0 && !gameOver)
+        {
+        gameOver = true;
+        GameObject gameOverText = GameObject.FindGameObjectWithTag("GameOver");
+        gameOverText.GetComponent<Animator>().SetBool("gameOver", true);
+        }
+        // 4 
+        for (int i = 0; i < healthIndicator.Length; i++)
+        {
+        if (i < Health)
+        {
+            healthIndicator[i].SetActive(true);
+        }
+        else
+        {
+            healthIndicator[i].SetActive(false);
+        }
+        }
+    }
+    }    
+
+    // Use this for initialization
+    void Start()
+    {
+        Gold = 1000;
+        Wave = 0;
+        Health = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
